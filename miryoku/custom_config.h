9d0548468bc0c553020497189ad925eb8379e4d7
Stefan Stefanache <stefan@stefanache.com>
2025-10-15 00:39:25 +0200

    Fix: Change LAYERMAPPING to LAYER

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,12 +2,8 @@
 
 #define NUMLOCK &kp KP_NUMLOCK
 
-#if defined (MIRYOKU_KEYBOARD_CORNE)
-
-#define MIRYOKU_LAYERMAPPING_BUTTON \
+#define MIRYOKU_LAYER_BUTTON \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         NUMLOCK,           NUMLOCK,           &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
-
-#endif
