e91d9fdc12b187ef82a52b893b3e877aacff2c6f
Stefan Stefanache <stefan@stefanache.com>
2025-10-15 00:21:20 +0200

    Add custom config where unused key of Corne right side is NUMLOCK

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,13 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
+// Update Button layer to toggle NUMLOCK on the unused key on the right side
 
+#define NUMLOCK &kp KP_NUMLOCK
+
+#if defined (MIRYOKU_KEYBOARD_CORNE)
+
+#define MIRYOKU_LAYERMAPPING_BUTTON \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              KP_NUMLOCK,        &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+
+#endif
